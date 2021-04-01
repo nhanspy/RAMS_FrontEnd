edab0a2a6de1b107f2e0828bcff5efd5eaad0ef2
nhanspy <nhan0095@gmail.com>
2021-04-01 10:42:09 +0700

    nhan them component cua nhan

---
+++ src/app/nhan/dat-ve/dat-ve.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dat-ve',
+  templateUrl: './dat-ve.component.html',
+  styleUrls: ['./dat-ve.component.css']
+})
+export class DatVeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
