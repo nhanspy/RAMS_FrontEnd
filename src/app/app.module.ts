1b10b6babd3eea4e97ddef67778db72bbaca13ba
kyokien99 <79929575+kyokien99@users.noreply.github.com>
2021-04-05 09:55:23 +0700

    kien push prototype

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { XemChiTietChuyenXeComponent } from './nhan/xem-chi-tiet-chuyen-xe/xem-chi-tiet-chuyen-xe.component';
 import { DatVeComponent } from './nhan/dat-ve/dat-ve.component';
+import { QlNguoiDungComponent } from './kien/ql-nguoi-dung/ql-nguoi-dung.component';
+import { QlTuyenXeComponent } from './kien/ql-tuyen-xe/ql-tuyen-xe.component';
+import { QlNhaXeComponent } from './kien/ql-nha-xe/ql-nha-xe.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     XemChiTietChuyenXeComponent,
-    DatVeComponent
+    DatVeComponent,
+    QlNguoiDungComponent,
+    QlTuyenXeComponent,
+    QlNhaXeComponent
   ],
   imports: [
     BrowserModule,
