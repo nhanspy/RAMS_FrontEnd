cc7ce358be88a2a18210ab4081b3fcaf7a889379
kyokien99 <79929575+kyokien99@users.noreply.github.com>
2021-04-06 14:05:23 +0700

    commit de chuyen nhanh

--- src/app/kien/ql-nguoi-dung/ql-nguoi-dung.component.ts
+++ src/app/kien/ql-nguoi-dung/ql-nguoi-dung.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as $ from 'jquery';
+//
+$('#elemId').width();
 @Component({
   selector: 'app-ql-nguoi-dung',
   templateUrl: './ql-nguoi-dung.component.html',
