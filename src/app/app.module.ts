4474d77f4af6a99b8e61c6fd7d4041beff81e517
nhanspy <nhan0095@gmail.com>
2021-04-12 23:36:12 +0700

    Ngon nhat cua kien

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DangKyComponent } from './thao/dang-ky/dang-ky.component';
-import { DangNhapComponent } from './thao/dang-nhap/dang-nhap.component';
 
 import { XemChiTietChuyenXeComponent } from './nhan/xem-chi-tiet-chuyen-xe/xem-chi-tiet-chuyen-xe.component';
 import { DatVeComponent } from './nhan/dat-ve/dat-ve.component';
-import { XemLichSuComponent } from './trong/xem-lich-su/xem-lich-su.component';
-import { TimKiemComponent } from './trong/tim-kiem/tim-kiem.component';
 import { QlNguoiDungComponent } from './kien/ql-nguoi-dung/ql-nguoi-dung.component';
 import { QlTuyenXeComponent } from './kien/ql-tuyen-xe/ql-tuyen-xe.component';
 import { QlNhaXeComponent } from './kien/ql-nha-xe/ql-nha-xe.component';
-import {KiemTraVeComponent} from './thao/kiem-tra-ve/kiem-tra-ve.component';
 
 @NgModule({
     declarations: [
@@ -22,12 +17,7 @@ import {KiemTraVeComponent} from './thao/kiem-tra-ve/kiem-tra-ve.component';
         DatVeComponent,
         QlNguoiDungComponent,
         QlTuyenXeComponent,
-        QlNhaXeComponent,
-        DangKyComponent,
-        DangNhapComponent,
-        KiemTraVeComponent,
-        XemLichSuComponent,
-        TimKiemComponent
+        QlNhaXeComponent
     ],
   imports: [
     BrowserModule,
