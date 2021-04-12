81b7cdb191df9bb00b72c090bbf191f76ae1ca6c
Trong <trongdtu99@gmail.com>
2021-04-12 20:16:27 +0700

    update root

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,14 +10,27 @@ import { XemChiTietChuyenXeComponent } from './nhan/xem-chi-tiet-chuyen-xe/xem-c
 import { DatVeComponent } from './nhan/dat-ve/dat-ve.component';
 import {HttpClientModule} from '@angular/common/http';
 import {XemChiTietChuyenXeService} from './nhan/service/xem-chi-tiet-chuyen-xe.service';
-<<<<<<< HEAD
+import { XemLichSuComponent } from './trong/xem-lich-su/xem-lich-su.component';
+import { TimKiemComponent } from './trong/tim-kiem/tim-kiem.component';
+import { QlNguoiDungComponent } from './kien/ql-nguoi-dung/ql-nguoi-dung.component';
+import { QlTuyenXeComponent } from './kien/ql-tuyen-xe/ql-tuyen-xe.component';
+import { QlNhaXeComponent } from './kien/ql-nha-xe/ql-nha-xe.component';
+import {KiemTraVeComponent} from './thao/kiem-tra-ve/kiem-tra-ve.component';
 import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     XemChiTietChuyenXeComponent,
-    DatVeComponent
+    DatVeComponent,
+    QlNguoiDungComponent,
+    QlTuyenXeComponent,
+    QlNhaXeComponent,
+    DangKyComponent,
+    DangNhapComponent,
+    KiemTraVeComponent,
+    XemLichSuComponent,
+    TimKiemComponent
   ],
     imports: [
         BrowserModule,
@@ -25,34 +38,6 @@ import {FormsModule} from '@angular/forms';
         HttpClientModule,
         FormsModule
     ],
-=======
-import { XemLichSuComponent } from './trong/xem-lich-su/xem-lich-su.component';
-import { TimKiemComponent } from './trong/tim-kiem/tim-kiem.component';
-import { QlNguoiDungComponent } from './kien/ql-nguoi-dung/ql-nguoi-dung.component';
-import { QlTuyenXeComponent } from './kien/ql-tuyen-xe/ql-tuyen-xe.component';
-import { QlNhaXeComponent } from './kien/ql-nha-xe/ql-nha-xe.component';
-import {KiemTraVeComponent} from './thao/kiem-tra-ve/kiem-tra-ve.component';
-
-@NgModule({
-    declarations: [
-        AppComponent,
-        XemChiTietChuyenXeComponent,
-        DatVeComponent,
-        QlNguoiDungComponent,
-        QlTuyenXeComponent,
-        QlNhaXeComponent,
-        DangKyComponent,
-        DangNhapComponent,
-        KiemTraVeComponent,
-        XemLichSuComponent,
-        TimKiemComponent
-    ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
-  ],
->>>>>>> 4d142d9b52648006e8ee212282e82a4073e75b2a
   providers: [
     XemChiTietChuyenXeService
   ],
